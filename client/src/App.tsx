ff267793b4335ceaece9961df2956d570b19791a
UzairInf <uzair.zeeshan1142@gmail.com>
2026-09-25 14:54:21 +0500

    Made the SignIn and SignUp Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "./Auth/SIgnIn";
+import SignUp from "./Auth/SignUp";
+
 function App() {
   return (
-    <></>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
